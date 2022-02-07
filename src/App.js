830d678d7f2a02ef3fd9ae3e9c9ac0e571bb26f3
KIRK SAHDO <ksahdo10@gmail.com>
2022-02-07 11:07:06 -0400

    upload files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+import Home from "./components/Home";
+
+const App = () => (
+
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
+    </div>
+    
+);
+
+export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB8DZf3S5MzJHEMj41m1E4r2nQl9etrmeI",
+  authDomain: "clone-3452a.firebaseapp.com",
+  projectId: "clone-3452a",
+  storageBucket: "clone-3452a.appspot.com",
+  messagingSenderId: "752115628456",
+  appId: "1:752115628456:web:e28dd29c3bc72c0e6a859d"
+};
+
+export default firebaseConfig; 
